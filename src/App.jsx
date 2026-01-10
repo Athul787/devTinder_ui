0cc39855a9deeacc352149dcafd069cbd45f9888
Athul787 <athulmbabu@gmail.com>
2026-01-10 19:35:16 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
