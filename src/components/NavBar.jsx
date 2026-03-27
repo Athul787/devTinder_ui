d3ea08272209ed54ddbc93de9957426cfcbce622
Athul787 <athulmbabu@gmail.com>
2026-03-27 23:49:27 +0530

    rename

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
-          DevTinder
+          DevFinder
         </Link>
       </div>
       <div className="flex gap-2">
